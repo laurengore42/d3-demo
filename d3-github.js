16a9abb03f21ddbb3d940d86b5bd1b4ab2796d6d
Lauren Gore <appledore.productions@gmail.com>
2018-07-20 15:31:45 +0100

    move scaling into colour scale

--- d3-github.js
+++ d3-github.js
@@ -69,7 +69,7 @@ var drawGithub = (function () {
             .attr("height", height);
         
         var color = d3.scaleOrdinal(d3.schemeCategory20);
-        var colorGrey = d3.scaleLinear().domain([0,19]).range(['snow', 'black']);
+        var colorGrey = d3.scaleLinear().domain([0,50]).range(['snow', 'black']);
         
         simulation = d3.forceSimulation()
             .force("link", d3.forceLink().id(function(d) { return d.id; }))
@@ -81,7 +81,7 @@ var drawGithub = (function () {
             .selectAll("line")
             .data(graph.links)
             .enter().append("line")
-            .attr("stroke", function (d) { return colorGrey(Math.floor((Math.sqrt(d.value))*(2/5))); })
+            .attr("stroke", function (d) { return colorGrey(Math.floor(Math.sqrt(d.value))); })
             .attr("stroke-width", 2);
 
         node = svg.append("g")
