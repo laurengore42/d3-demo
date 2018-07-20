f315834a51ab36c3badb84d6595b9a8f13f7186a
Lauren Gore <appledore.productions@gmail.com>
2018-07-20 10:28:00 +0100

    construct points over time graph, add WLD data

--- d3-footie.js
+++ d3-footie.js
@@ -25,12 +25,16 @@ var drawGithub = (function () {
             t.played = 0;
             t.goalsFor = 0;
             t.goalsAgainst = 0;
+            t.win = 0;
+            t.draw = 0;
+            t.lose = 0;
+            t.scoreGraph = [];
             teams[t.id] = t;
         });
 
         // run through season and establish points ranking
         var dataFromJson = loadData();
-        dataFromJson.forEach(function (match) {
+        dataFromJson.sort(compareMatchday).forEach(function (match) {
             var homeTeam = teams[match.homeTeam.id];
             var awayTeam = teams[match.awayTeam.id];
             homeTeam.played++;
@@ -41,14 +45,22 @@ var drawGithub = (function () {
             awayTeam.goalsAgainst += match.score.fullTime.homeTeam;
             if (match.score.winner === "HOME_TEAM") {
                 homeTeam.points += 3;
+                homeTeam.win++;
+                awayTeam.lose++;
             }
             else if (match.score.winner === "AWAY_TEAM") {
                 awayTeam.points += 3;
+                awayTeam.win++;
+                homeTeam.lose++;
             }
             else {
                 homeTeam.points += 1;
+                homeTeam.draw++;
                 awayTeam.points += 1;
+                awayTeam.draw++;
             }
+            homeTeam.scoreGraph[match.matchday] = homeTeam.points;
+            awayTeam.scoreGraph[match.matchday] = awayTeam.points;
         });
         teams.forEach(function (team) {
             team.goalDiff = team.goalsFor - team.goalsAgainst;
@@ -59,6 +71,9 @@ var drawGithub = (function () {
 
         standings = teams.sort(compareTeamStandings);
         console.log(standings);
+    }
+    function compareMatchday(a, b) {
+        return a.matchday > b.matchday ? 1 : a.matchday < b.matchday ? -1 : 0;
     }
 
     function compareTeamStandings(a, b) {
